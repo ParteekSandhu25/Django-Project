d36e530bc0445091949d8b28aa9e604d032a3c3f
Parteek Sandhu <parteek.sandhu@qvantel.com>
2025-06-17 13:46:57 +0530

    added new app follower

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.views.generic import DeleteView
+from feed.models import Post
 
 class ProfileDetailView(DeleteView):
     http_method_names = ['get']
@@ -8,3 +9,9 @@ class ProfileDetailView(DeleteView):
     context_object_name = 'user' 
     slug_field = "username"
     slug_url_kwarg = "username"
+
+    def get_context_data(self, **kwargs):
+        user = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context['total_posts'] = Post.objects.filter(author=user).count()
+        return context
