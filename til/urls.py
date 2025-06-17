903825ab8b72e0b67c0d312f9552812afc427b6f
Parteek Sandhu <parteek.sandhu@qvantel.com>
2025-06-17 13:11:01 +0530

    added a profiles view and urls

--- til/urls.py
+++ til/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from feed import urls as feed_urls
+from profiles import urls as profiles_urls
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include(feed_urls, namespace='feed')),
+    path("profiles/", include(profiles_urls, namespace='profiles')),
     path("", include("allauth.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
